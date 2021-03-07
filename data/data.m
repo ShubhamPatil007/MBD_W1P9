ca51ab6e7e98ad2819512460f4709cb811483d18
ShubhamPatil007 <36992441+ShubhamPatil007@users.noreply.github.com>
2021-03-08 00:05:43 +0530

    Initial commit, Complete.

---
+++ data/data.m
@@ -0,0 +1,13 @@
+L = 0.133; % motor inductance
+R = 0.136; % resistance of the stator windings
+lambda = 50; % amplitude of the flux induced by magnet of the rotor in stator phases
+J = 1.33; % inertia of rotor and load
+Fw = 1.5; % viscous friction of model
+
+
+
+% Te  electromagnetic torque
+% vd  id voltage and current in d-direction
+% vq  iq voltage in q-direction
+% Tm mechanical torque
+% w angular velocity of motor
---
+++ startup.m
@@ -0,0 +1,5 @@
+run data/data.m;
+
+sim('models/BLDC_motor.slx');
+
+run models/BLDC_motor.slx;
